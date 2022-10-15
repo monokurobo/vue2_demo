0c328bd9088912b91782a63eb759220a51f7205d
veronica <87801036@qq.com>
2022-10-15 22:51:12 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: './src/main.js', // 修改入口文件
+    }
+  },
+  lintOnSave: false // 关闭代码检查
 })
