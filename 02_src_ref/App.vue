98d2daae255bc0742c296dd1575b510f65f7c08a
veronicaJun <87801036@qq.com>
2022-10-16 13:06:57 +0800

    ✨ feat: ref

---
+++ 02_src_ref/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1 v-text="msg" ref="h1"></h1>
+    <button @click="shwoDom">点我输入 dom</button>
+    <School></School>
+  </div>
+</template>
+
+<script>
+import School from './components/School'
+
+export default {
+  name: 'App',
+  components: {
+    School
+  },
+  data () {
+    return {
+      msg: 'Hello ref'
+    }
+  },
+  methods: {
+    shwoDom () {
+      console.log('Hello ref',this.$refs.h1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02_src_ref/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{name}}</h2>
+    <h2>{{adress}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'school',
+    data() {
+      return {
+        name: '尚硅谷',
+        adress: '北京',
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
