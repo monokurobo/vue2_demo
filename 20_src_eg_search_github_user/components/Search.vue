70a78b4b96ebfd9358d0aa536ee78123fcffd219
veronicaJun <87801036@qq.com>
2022-10-17 21:04:48 +0800

    ✨ feat: 20_search_user

---
+++ 20_src_eg_search_github_user/components/Search.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+        <input type="text" placeholder="enter the name you search" v-model="keyword"/>
+        <button @click="searchUsers">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'Search',
+    data () {
+        return {
+            keyword: ''
+        }
+    },
+    methods:{
+        searchUsers(){
+            this.$bus.$emit('recievedUsersDate', {
+                isFirst: false,
+                isLoading: true,
+                errMsg: '',
+                users: []
+            })
+            axios.get(`https://api.github.com/search/users?q=${this.keyword}`).then(
+                res => {
+                    this.$bus.$emit('recievedUsersDate', {
+                        isLoading: false,
+                        errMsg: '',
+                        users: res.data.items
+                    })
+                },
+                err => {
+                    this.$bus.$emit('recievedUsersDate', {
+                        isLoading: false,
+                        errMsg: err.message,
+                        users: []
+                    })
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
