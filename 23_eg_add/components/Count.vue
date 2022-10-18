96d19587b1210f4af745c01e813a0cc90fb3bb17
veronicaJun <87801036@qq.com>
2022-10-18 13:34:38 +0800

    ✨ feat: 23_eg_add

---
+++ 23_eg_add/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>当前求和为{{sum}}</h1>
+    <select name="" id="" v-model.number="n">
+      <option v-for="item in 3" :key="item" :value="item">{{item}}</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数时加</button>
+    <button @click="incrementWait">等一等再加</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Count',
+    data () {
+        return {
+          n: 1, // 用户选择的数字
+          sum: 0  //当前的和
+        }
+      },
+      methods: {
+        increment () {
+          this.sum += this.n
+        },
+        decrement () {
+          this.sum -= this.n
+        },
+        incrementOdd () {
+          if(this.sum % 2){
+            this.sum += this.n
+          }
+        },
+        incrementWait () {
+          setTimeout(() => {
+            this.increment()
+          },500)
+        }
+      }
+}
+</script>
+
+<style>
+button {
+  margin-left: 5px;
+}
+</style>
