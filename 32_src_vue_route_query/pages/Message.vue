484736e22f35d5a6b7f433d6a946b2b9663dc969
veronicaJun <87801036@qq.com>
2022-10-24 19:40:07 +0800

    ✨ feat: 32_router_query

---
+++ 32_src_vue_route_query/pages/Message.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="(m, index) in 3" :key="index">
+				<router-link 
+					:to="{
+						path: '/home/message/detail',
+						query: {
+							id: m,
+							title: index
+						}
+					}"
+				>
+					{{m}}
+				</router-link>&nbsp;&nbsp;
+			</li>
+		</ul>
+		<hr>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'Message'
+}
+</script>
