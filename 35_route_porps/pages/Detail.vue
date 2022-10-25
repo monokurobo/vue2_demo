2e906680772b896aecfe11f5b373373263ac4da1
veronicaJun <87801036@qq.com>
2022-10-25 09:42:01 +0800

    ✨ feat: 35_route_props

---
+++ 35_route_porps/pages/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<ul>
+			<!-- query -->
+			<!-- <li>消息编号:{{$route.query.id}}</li>
+			<li>消息标题:{{$route.query.title}}</li> -->
+			
+			<!-- params -->
+			<li>消息编号:{{id}}</li>
+			<li>消息标题:{{title}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['id','title'],
+		name:'Detail',
+		mounted(){
+			console.log(this)
+		}
+	}
+</script>
