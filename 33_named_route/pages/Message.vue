42a3d0cb8884219dd0bf6e55ad70584711adb3e5
veronicaJun <87801036@qq.com>
2022-10-25 09:11:40 +0800

    ✨ feat: 33_named_route

---
+++ 33_named_route/pages/Message.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="(m, index) in 3" :key="index">
+				<router-link 
+					:to="{
+						// path: '/home/message/detail',
+						name: 'detail',
+						query: {
+							id: m,
+							title: index
+						}
+					}"
+				>
+					{{m}}
+				</router-link>&nbsp;&nbsp;
+			</li>
+		</ul>
+		<hr>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'Message'
+}
+</script>
